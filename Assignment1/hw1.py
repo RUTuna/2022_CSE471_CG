f6722c94ae63268e7d061fca76a1f561ffc4745a
jian <p000207jian@gmail.com>
2022-09-28 11:45:46 +0900

    ASG1) homogenous coordinate로 변경, ellipse 그리는 방식 변경, code refactoring

--- Assignment1/hw1.py
+++ Assignment1/hw1.py
@@ -7,41 +7,39 @@ import math
 class Polygon:
     def __init__(self, x, y):
         self.mat = np.eye(4)
-        self.x = x
-        self.y = y
+        self.origin = [x, y, 1, 1]
+        self.newOrigin = [x, y, 1, 1]
         self.vertices = []
 
+    # Task 1, Task 4
     def draw(self):
+        # draw polygon
         glBegin(self.mode)
         glColor4f(self.color[0], self.color[1], self.color[2], self.color[3])
         for vertex in self.vertices:
-            newVertex = self.mat @ vertex
+            newVertex = self.mat @ vertex # apply transform matrix
             glVertex3f(newVertex[0], newVertex[1], newVertex[2])
+        glEnd()
 
-
-            # for vertex in self.vertices:
-            # print(f"origin {vertex} {self.mat}")
-            # newVertex = np.transpose([vertex,[0,0,0,0],[0,0,0,0],[0,0,0,0]])
-            # print(f"trans {newVertex}")
-            # newVertex = newVertex @ self.mat
-            # print(f"mat {newVertex}")
-            # newVertex = np.transpose(newVertex)[0]
-            # print(f"new {newVertex}")
-            # glVertex3f(newVertex[0], newVertex[1], newVertex[2])
+        # draw origin point
+        glPointSize(2.0)
+        glBegin(GL_POINTS)
+        glColor4f(1,1,1,1)
+        glVertex3f(self.newOrigin[0], self.newOrigin[1], self.newOrigin[2])
         glEnd()
 
     # Task 3-1
     def scale(self, sx, sy):
         dx = dy = 1
-        if sx > 0 :
-            dx = 1.1
+        if sx > 0:
+            dx = 1.01
         elif sx < 0:
-            dx = 0.9
+            dx = 0.99
         
-        if sy > 0 :
-            dy = 1.1
+        if sy > 0:
+            dy = 1.01
         elif sy < 0:
-            dy = 0.9
+            dy = 0.99
 
         transfromeMat = np.array([[dx,  0,  0,  0], 
                                   [0,   dy, 0,  0], 
@@ -58,27 +56,16 @@ class Polygon:
                                   [sin, cos,    0,  0], 
                                   [0,   0,      1,  0], 
                                   [0,   0,      0,  1]])
-        # xm = np.array([[cos, -sin,   0,  0], 
-        #                           [sin, cos,    0,  0], 
-        #                           [0,   0,      1,  0], 
-        #                           [0,   0,      0,  1]])
-        # ym = np.array([[cos, -sin,   0,  0], 
-        #                           [sin, cos,    0,  0], 
-        #                           [0,   0,      1,  0], 
-        #                           [0,   0,      0,  1]])
-                                
 
         self.mat = transfromeMat @ self.mat
 
     # Task 3-3
     def translation(self, dx, dy):
-        transfromeMat = np.array([[1,   0,  dx,  dx], 
-                                  [0,   1, dy,  dy], 
-                                  [0,   0,  1,  0], 
-                                  [0,   0,  0,  1]])
+        transfromeMat = np.array([[1,   0,  dx,  0], 
+                                  [0,   1,  dy,  0], 
+                                  [0,   0,  1,   0], 
+                                  [0,   0,  0,   1]])
         self.mat = transfromeMat @ self.mat
-        # for i, vertex in enumerate(self.vertices):
-        #     self.vertices[i] = [vertex[0] + dx, vertex[1] + dy,  vertex[2], vertex[3]]
 
 
 # Task 1
@@ -87,17 +74,17 @@ class Triangle(Polygon):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.mode = GL_TRIANGLES
-        self.color = [55/255, 114/255, 240/255, 1]
+        self.color = [186/255, 231/255, 175/255, 1]
         self.vertices = [
-            [x, y+0.1, 0, 1],
-            [x-0.1, y-0.1, 0, 1],
-            [x+0.1, y-0.1, 0, 1],
+            [x, y+0.1, 1, 1],
+            [x-0.1, y-0.1, 1, 1],
+            [x+0.1, y-0.1, 1, 1],
         ]
         self.vertices = list(np.array(self.vertices))
 
     def draw(self):
         super().draw()
-
+        
     def translation(self, dx, dy):
         super().translation(dx, dy)
     
@@ -112,12 +99,12 @@ class Rectangle(Polygon):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.mode = GL_QUADS
-        self.color = [148/255, 74/255, 255/255, 1]
+        self.color = [175/255, 196/255, 231/255, 1]
         self.vertices = [
-            [x-0.1, y+0.1, 0, 1],
-            [x+0.1, y+0.1, 0, 1],
-            [x+0.1, y-0.1, 0, 1],
-            [x-0.1, y-0.1, 0, 1],
+            [x-0.1, y+0.1, 1, 1],
+            [x+0.1, y+0.1, 1, 1],
+            [x+0.1, y-0.1, 1, 1],
+            [x-0.1, y-0.1, 1, 1],
         ]
         self.vertices = list(np.array(self.vertices))
 
@@ -134,29 +121,21 @@ class Rectangle(Polygon):
         super().rotation(degree)
 
 
-# TODO : 대칭 추가하기, 크기 바꾸고, 변수 이름 변경 
 class Ellipse(Polygon):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.mode = GL_TRIANGLE_FAN
-        self.color = [24/255, 158/255, 135/255, 1]
+        self.color = [238/255, 175/255, 175/255, 1]
         self.vertices = []
-        pointNum = 20
-        theta = (2 * math.pi) / pointNum
-        c = math.cos(theta)
-        s = math.sin(theta)
-        xs, ys = 1, 0 # starting angle is 0
-        for i in range(pointNum):
-            pointX = xs * 0.05 + x
-            pointY = ys * 0.01 + y
-            self.vertices.append([pointX, pointY, 0, 1])
-            # self.vertices.extend([[pointX, pointY, 0],[-pointX, pointY, 0], [pointX, -pointY, 0], [-pointX, -pointY, 0]])
-            
-            t = xs
-            xs = c * xs - s * ys
-            ys = s * t + c * ys
 
-        xs = ys = t = 0
+        pointNum = 40
+        radiusX = 0.1
+        radiusY = 0.05
+        theta = math.pi / pointNum
+        for i in range(pointNum):
+            pointX = radiusX * math.cos(theta * i)
+            pointY = radiusY * math.sin(theta * i) 
+            self.vertices.extend([[x + pointX, y + pointY, 1, 1], [x - pointX, y + pointY, 1, 1], [x -pointX, y - pointY, 1, 1], [x + pointX, y - pointY, 1, 1]])
 
     def draw(self):
         super().draw()
@@ -174,13 +153,17 @@ class Ellipse(Polygon):
 class Viewer:
     def __init__(self):
         self.mode = 0
-        self.polygons = []
-        self.polyNum = 0
-        self.halfWidth = 400
+        self.polygons = [] # array to save added polygon
+        self.polyNum = 0 # number of polygons
+
+        # half of window width, height 
+        self.halfWidth = 400 
         self.halfHeight = 400
-        self.click = False
-        self.isScaleMode = False
-        pass
+
+        # init is rotate, global mode
+        self.click = False # mouse click
+        self.isScaleMode = False # True : scale, Flase : rotate
+        self.isGlobalMode = True # True : global transform, Flase : local transform
 
     def display(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -192,6 +175,7 @@ class Viewer:
         self.halfWidth = glutGet(GLUT_WINDOW_WIDTH)/2
         self.halfHeight = glutGet(GLUT_WINDOW_HEIGHT)/2
 
+        # Task 5
         for pol in self.polygons:
             pol.draw()
 
@@ -206,6 +190,8 @@ class Viewer:
         if glutGetModifiers() & GLUT_ACTIVE_CTRL:
             print("ctrl pressed")
 
+        # Task 2
+        # change draw mode
         if key == b'1':
             self.mode = 1
             print("Triangle Draw")
@@ -219,12 +205,19 @@ class Viewer:
             self.mode = 0
             print("cancellation of polygon drawing mode")
         
+
+        # change scale/rotate global/local mode
         if key == b's':
             self.isScaleMode = not self.isScaleMode
             print(f"now scale mode is {self.isScaleMode}")
+
+        if key == b'g':
+            self.isGlobalMode = not self.isGlobalMode
+            print(f"now global mode is {self.isGlobalMode}")
     
         glutPostRedisplay()
 
+    # Task 3-3, Task 4
     def special(self, key, x, y):
         print(f"special key event: key={key}, x={x}, y={y}")
         if key == 100: # left arrow
@@ -253,6 +246,7 @@ class Viewer:
         # button macros: GLUT_LEFT_BUTTON, GLUT_MIDDLE_BUTTON, GLUT_RIGHT_BUTTON
         print(f"mouse press event: button={button}, state={state}, x={x}, y={y}")
 
+        # set start coordinates to measure drag distance
         if state == GLUT_DOWN:
             self.click = True
             self.xStart = x
@@ -260,44 +254,57 @@ class Viewer:
         elif state == GLUT_UP:
             self.click = False
         
+        # Task 2
         if button == GLUT_LEFT_BUTTON and self.click:
+            # conversion between window coordinates and world coordinates
             newX = (x-self.halfWidth)/self.halfWidth
             newY = -(y-self.halfHeight)/self.halfHeight
-            self.polyNum =  self.polyNum + 1
-            if self.mode == 1 :
-                self.polygons.append( Triangle(newX,newY) )
-                print(f"add Triangle {self.polyNum}")
-            elif self.mode == 2 :
-                self.polygons.append( Rectangle(newX,newY) )
-                print(f"add Rectangle {self.polyNum}")
-            elif self.mode == 3 :
-                self.polygons.append( Ellipse(newX,newY) )
-                print(f"add Ellipse {self.polyNum}")
-            else :
-                 self.polyNum =  self.polyNum - 1
-                
 
+            if self.mode == 1:
+                self.polyNum =  self.polyNum + 1
+                self.polygons.append( Triangle(newX,newY) )
+                print(f"add Triangle, # polygon is {self.polyNum}")
+            elif self.mode == 2:
+                self.polyNum =  self.polyNum + 1
+                self.polygons.append( Rectangle(newX,newY) )
+                print(f"add Rectangle, # polygon is {self.polyNum}")
+            elif self.mode == 3:
+                self.polyNum =  self.polyNum + 1
+                self.polygons.append( Ellipse(newX,newY) )
+                print(f"add Ellipse, # polygon is {self.polyNum}")
+                
         glutPostRedisplay()
 
     def motion(self, x, y):
         print(f"mouse move event: x={x}, y={y}")
 
+        # drag distance
         dx = (self.xStart - x)
         dy = (self.yStart - y)
 
-        if self.isScaleMode :
-            if abs(dx) > 2 or abs(dy) > 2:
-                for pol in self.polygons:
+        # Task 3-1, Task 3-2, Task 4
+        if abs(dx) > 2 or abs(dy) > 2:
+            for pol in self.polygons:
+                if not self.isGlobalMode:
+                    pol.translation(-pol.newOrigin[0], -pol.newOrigin[1])
+
+                if self.isScaleMode:
                     pol.scale(dx, dy)
-        else :
-            if abs(dx) > 2 or abs(dy) > 2:
-                degree = math.pi/1800
-                if abs(dx) > abs(dy):
-                    degree = degree*dx
-                else :
-                    degree = -degree*dy
-                for pol in self.polygons:
+
+                else:
+                    degree = math.pi/1800
+                    if abs(dx) > abs(dy):
+                        degree = degree*dx
+                    else:
+                        degree = -degree*dy
                     pol.rotation(degree)
+
+                if not self.isGlobalMode:
+                    pol.translation(pol.newOrigin[0], pol.newOrigin[1])
+
+                # if global mode, transform origin
+                if self.isGlobalMode:
+                    pol.newOrigin = pol.mat @ pol.origin 
 
         glutPostRedisplay()
 
@@ -306,7 +313,7 @@ class Viewer:
         glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
         glutInitWindowSize(800, 800)
         glutInitWindowPosition(0, 0)
-        glutCreateWindow("CS471 Computer Graphics #1")
+        glutCreateWindow(b"CS471 Computer Graphics #1")
 
         glutDisplayFunc(self.display)
         glutKeyboardFunc(self.keyboard)
